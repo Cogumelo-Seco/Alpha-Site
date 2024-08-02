f15254752bd9511a282d010ce742b99844e4f25c
Cogu <wfrwellington13@gmail.com>
2024-08-02 17:59:23 -0300

    .

---
+++ pages/br/terms-of-service.js
@@ -0,0 +1,36 @@
+import { useEffect } from 'react';
+import cookies from 'next-cookies';
+import { useRouter } from 'next/router';
+import Head from "next/head"
+
+const load = () => {
+    const router = useRouter()
+    useEffect(async () => {
+        
+    })
+    return (
+        <html lang="pt-BR">
+            <head>
+                <meta property="theme-color" content="#010101" />
+                <meta property="og:title" content="Alpha Bot" />
+                <meta property="og:site_name" content="Alpha Site" />
+                <meta property="og:url" content="https://alpha-site.vercel.app/" />                
+                <meta property="og:image" content="/avatar/Roxo.png" />
+                <meta property="og:description" content="Olá eu sou o Alpha Bot, um bot com sistemas de rank para deixar todos ativos e competitivos para o 1° lugar" />
+            </head>
+
+            <style>{`
+                h1 {
+                    margin: 20px auto;
+                    font-size: 25px;
+                }
+            `}</style>
+
+            <body id="main">
+                <h1>Não existe termos ainda, TMJ</h1>
+            </body>
+        </html>
+    )
+}
+
+export default load
